10e1cbb590a359f8f29986f5468e654db3d099eb
WilliamMatiasDeOliveira <williammatiadeoliveira@gmail.com>
2026-03-22 11:23:17 -0300

    retornando o cuidador com base na especialidade escolhida (em andamento)

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Caregiver;
 use App\Models\Specialty;
 use Illuminate\Http\Request;
 
@@ -19,13 +20,29 @@ class ClientController extends Controller
     public function loadCaregivers(Request $request)
     {
         // RECEBE O ID DA ESPECIALIDADE ESCOLHIDA
-        $cuidados_pessoais = $request->cuidados_pessoais;
+        $specialtyId =
+            $request->cuidados_pessoais ??
+            $request->saude ??
+            $request->acompanhamento ??
+            $request->cuidados_especializado;
+
+        if (empty($specialtyId)) {
+            return redirect()->route('select.specialty')->with("error", "Selecione ao menos uma especialidade para buscar cuidadores");
+        }
 
         // BUSCAR CUIDADORES QUE TEM ESTA ESPECIALIDADE CADASTRADA
-
+        $caregivers = Caregiver::whereHas('specialties', function ($query) use ($specialtyId) {
+            $query->where('specialty_id', $specialtyId);
+        })
+            ->with([
+                'user',
+                'specialties' => function ($query) use ($specialtyId) {
+                    $query->where('specialty_id', $specialtyId);
+                }
+            ])
+            ->paginate(10);
 
         // RETORNAR OS CUIDADORES ENCONTRADOS PARA PAGINA select-specialty
-
-        dd($cuidados_pessoais);
+       return redirect()->route('select.specialty')->with('caregivers', $caregivers);
     }
 }
